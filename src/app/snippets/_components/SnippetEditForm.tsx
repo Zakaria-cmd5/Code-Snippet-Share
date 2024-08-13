59a75531bdff2c0b60828b224755a3b8977e835d
Zakaria Asad <zasad2587@gmail.com>
2024-08-13 15:34:08 +0300

    Handling the snippet ubdate

--- src/app/snippets/_components/SnippetEditForm.tsx
+++ src/app/snippets/_components/SnippetEditForm.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import { Editor } from "@monaco-editor/react";
+import { editSnippet } from "@/actions";
+import Editor from "@monaco-editor/react";
 import type { Snippet } from "@prisma/client";
+import { useState } from "react";
 
 interface Props {
   snippet: Snippet;
 }
 
 const SnippetEditForm = ({ snippet }: Props) => {
+  const [code, setCode] = useState(snippet.code);
+
+  const editorChangeHandler = (value: string = "") => {
+    setCode(value);
+  };
+
+  const editSnippetAction = editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
       <Editor
@@ -16,7 +26,13 @@ const SnippetEditForm = ({ snippet }: Props) => {
         language="javascript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={editorChangeHandler}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded mt-2">
+          Save
+        </button>
+      </form>
     </div>
   );
 };
