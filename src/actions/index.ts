0c5e58bb393d6eb58575b4c6703d67dfc4dd6f2e
Zakaria Asad <zasad2587@gmail.com>
2024-08-14 14:04:51 +0300

    Adding form validation using formState Hook

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,32 @@ export const deleteSnippet = async (id: number) => {
 
   redirect("/");
 };
+
+export const createSnippet = async (
+  fromState: { message: string },
+  formData: FormData
+) => {
+  const title = formData.get("title");
+  const code = formData.get("code");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return {
+      message: "Title must be longer",
+    };
+  }
+
+  if (typeof code !== "string" || code.length < 10) {
+    return {
+      message: "Code must be longer",
+    };
+  }
+
+  await db.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  redirect("/");
+};
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,25 +1,13 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+
+import { createSnippet } from "@/actions";
+import { useFormState } from "react-dom";
 
 const SnippetsNewPage = () => {
-  async function createSnippet(formData: FormData) {
-    "use server";
-
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    redirect("/");
-  }
+  const [formState, action] = useFormState(createSnippet, { message: "" });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -42,6 +30,11 @@ const SnippetsNewPage = () => {
             id="code"
           />
         </div>
+        {formState.message && (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400 text-center">
+            {formState.message}
+          </div>
+        )}
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
