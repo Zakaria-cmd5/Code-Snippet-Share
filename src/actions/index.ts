32262997532b0b78306bcf6002eca63601948eac
Zakaria Asad <zasad2587@gmail.com>
2024-08-14 21:37:32 +0300

    Fix the cache issue

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/db";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export const editSnippet = async (id: number, code: string) => {
@@ -17,6 +18,7 @@ export const deleteSnippet = async (id: number) => {
     where: { id },
   });
 
+  revalidatePath("/");
   redirect("/");
 };
 
@@ -58,5 +60,6 @@ export const createSnippet = async (
     }
   }
 
+  revalidatePath("/");
   redirect("/");
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,15 @@ export default async function Home() {
     </Link>
   ));
 
-  return(
+  return (
     <div>
       <div className="flex justify-between m-2 items-center">
         <h1 className="text-xl font-bold">Snippets</h1>
-        <Link href='/snippets/new' className="border p-2 rounded">New Snippent</Link>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New Snippent
+        </Link>
       </div>
-    <div className="flex flex-col gap-2 mt-3">{renderSnippets}</div>
+      <div className="flex flex-col gap-2 mt-3">{renderSnippets}</div>
     </div>
-    )
+  );
 }
